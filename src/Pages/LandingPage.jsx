ffb4f8c7d11cb26b8d78d44fb8a52d92829e6986
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-04-15 17:58:54 +0530

    Create LandingPage.jsx

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import "../stylesheets/landing-page.css"
+import { heroLogo } from "../assets/images"
+
+function LandingPage() {
+return (
+    <>
+    <main>
+        <img className="img-hero" src={heroLogo} alt="" />
+        <div className="section-hero">
+        <h1>Quizingoo</h1>
+        <h2>Playing and creating quizzes <br/> has never been more easy & fun with <br/><span>Quizingoo!!</span></h2>
+
+        <Link className='btn btn-launch btn-primary-solid' to="/HomePage">Browse Quizzes <i class="fas fa-arrow-circle-right"></i></Link>
+        </div>
+    </main>
+    </>
+  );
+}
+
+export { LandingPage };
