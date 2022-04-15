05304bbbc1eb950524ad78ba14d53a77d86dd12b
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-04-15 17:58:50 +0530

    Create HomePage.jsx

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,31 @@
+import { CategoryCard } from "./Category/category-card";
+import { Navigation, Footer } from "../components/Components";
+import "../stylesheets/homepage.css"
+
+function HomePage() {
+  return (
+    <>
+    <Navigation />
+      <div className="section-top">
+        <h1 className="head-top">Quizingoo</h1>
+
+        <h2 className="desc-top">Select a Category!</h2>
+      </div>
+      <div className="container-card">
+        {categoryList &&
+          categoryList.map((item) => {
+            return (
+              <CategoryCard
+                key={item.value}
+                value={item.value}
+                item={item.category}
+              />
+            );
+          })}
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export { HomePage };
