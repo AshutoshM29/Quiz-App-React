d875b39f7c68d87eff881da046dc3a81099fd0d2
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-04-15 16:59:56 +0530

    Create Navigation.jsx

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,64 @@
+import { Link } from 'react-router-dom'
+import "./navigation.css"
+import {
+qLogo,
+} from '../../assets/images'
+
+function Navigation() {
+return (
+<nav className="nav-header nav-bar">
+    <div className="nav-section">
+        <div className="nav-section-items">
+            <Link to="/LandingPage">
+            <img src={qLogo} className="nav-logo" loading="eager" alt="Logo" />
+            </Link>
+        </div>
+        <ul className="nav-bar-links nav-section-items">
+            <li className="list-inline-item">
+                <a className="link" href="/trending">Trending</a>
+            </li>
+            <li className="list-inline-item">
+                <a className="link" href="/new">New</a>
+            </li>
+            <li className="list-inline-item">
+                <a className="link" href="/grooming">For You</a>
+            </li>
+        </ul>
+    </div>
+    <div className="nav-section">
+        <div className="section-end">
+        <label className="search-bar">
+            <span className="search-bar-btn" type="submit">
+                <i className="fa fa-search"></i>
+            </span>
+            <input className="search-bar-input" type="text" placeholder="Search a quiz!" name="search"></input>
+        </label>
+        <ul className="nav-icons">
+            <li className="list-inline-icons">
+                <Link className="nav-icon-link" to="/Signup">
+                <span className="nav-icon">
+                    <i className="fas fa-user-plus"></i>
+                </span>
+                <span className="nav-icon-text">
+                    Sign Up
+                </span>
+                </Link>
+            </li>
+            <li className="list-inline-icons">
+                <Link className="nav-icon-link" to="/">
+                <span className="nav-icon">
+                    <i className="fas fa-user"></i>
+                </span>
+                <span className="nav-icon-text">
+                    Profile
+                </span>
+                </Link>
+            </li>
+        </ul>
+    </div>
+    </div>
+</nav>
+)
+}
+
+export {Navigation}
