0b79cb216840b5bd834816ab64ce73b8f8681532
Ashutosh Mishra <89376505+AshutoshM29@users.noreply.github.com>
2022-10-28 12:32:37 +0530

    update Landing page to typescript

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -9,9 +9,16 @@ return (
         <img className="img-hero" src={heroLogo} alt="" />
         <div className="section-hero">
         <h1>Quizingoo</h1>
-        <h2>Playing and creating quizzes <br/> has never been more easy & fun with <br/><span>Quizingoo!!</span></h2>
+        <h2>Playing and creating quizzes <br/> has never been more easy & fun with <br/>
+          <span>Quizingoo!!</span>
+        </h2>
 
-        <Link className='btn btn-launch btn-primary-solid' to="/HomePage">Browse Quizzes <i class="fas fa-arrow-circle-right"></i></Link>
+        <Link
+            className='btn btn-launch btn-primary-solid'
+            to="/Homepage">
+              Browse Quizzes
+            <i className="fas fa-arrow-circle-right"></i>
+        </Link>
         </div>
     </main>
     </>
